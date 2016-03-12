1df4cb4127377641975dadcfa00137f5c70c772b
BryceX <btau20102@yahoo.com>
2016-03-11 17:46:30 -0800

    did some ai stuff

---
+++ Gachatown stuff/Assets/Scripts/GachaToyAI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GachaToyAI : MonoBehaviour
+{
+    Transform tr;
+    public Transform target;
+    public bool isSeeking;
+    public bool isWandering;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+
+    }
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        //if (isWandering)
+        //{
+        //    TrueWander();
+        //}     
+        if (isSeeking)
+        {
+            Seek();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            isWandering = false;
+            isSeeking = true;
+        }
+
+    }
+    void TrueWander()
+    {
+        var foo = transform.rotation.eulerAngles;
+        foo.x = 0;
+        foo.z = 0;
+        foo.y += Random.Range(-3, 5);
+        tr.Translate(transform.forward * .002f);
+        tr.transform.rotation = Quaternion.Euler(foo);
+    }
+    void Seek()
+    {
+        tr.Translate(Vector3.Normalize(tr.transform.position -target.transform.position)*.02f);
+    }
+
+
+    void OnCollisionEnter(Collision myTarget)
+    {
+        if (myTarget.gameObject.tag == "Target")
+        {
+            Debug.Log("IHITTHETHING");
+        }
+
+    }
+}
