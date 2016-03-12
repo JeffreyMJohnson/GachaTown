8ae4e52389aeffa598a82ea9f39d18cf480708b6
Jeffrey Johnson <jefjohms@gmail.com>
2016-03-12 15:38:19 -0800

    reading and writing a prefab to local database proof of concept.

---
+++ Assets/_scripts/DbTest.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+using Assets._scripts;
+
+
+public class DbTest : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        Gacha myGacha = new Gacha();
+        myGacha.name = "FooBar";
+        myGacha.prefab = myPrefab;
+
+        string gachaString = JsonUtility.ToJson(myGacha);
+
+
+        string conn = "URI=file:" + Application.dataPath + "/Database/TestDB1.db";//path to database
+        IDbConnection dbConn = (IDbConnection)new SqliteConnection(conn);
+        dbConn.Open();//open connection to database
+        IDbCommand dbCmd = dbConn.CreateCommand();
+
+        string query = string.Format("UPDATE User SET Gacha='{0}' WHERE Name='Foo'", gachaString);
+        dbCmd.CommandText = query;
+        dbCmd.ExecuteNonQuery();
+        query = "SELECT ID, Name, Gacha FROM User WHERE Gacha NOT NULL";
+        dbCmd.CommandText = query;
+        IDataReader reader = dbCmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            string gacha = "";
+            gacha = reader.GetString(2);
+            
+
+
+            Debug.Log(string.Format("ID: {0}\nName: {1}\nGacha: {2}", id, name, gachaString));
+        }
+        reader.Close();
+        reader = null;
+        dbCmd.Dispose();
+        dbCmd = null;
+        dbConn.Close();
+        dbConn = null;
+
+        Gacha myNewGacha = JsonUtility.FromJson<Gacha>(gachaString);
+
+        Instantiate<GameObject>(myNewGacha.prefab);
+    }
+}
---
+++ Assets/_scripts/Gacha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets._scripts
+{
+    [Serializable]
+    class Gacha
+    {
+        public string name;
+
+        public GameObject prefab;
+
+    }
+}
