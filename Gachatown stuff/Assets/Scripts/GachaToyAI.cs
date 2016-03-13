4dfde637297f816a88fca7390b4bba2ec9c45c19
BryceX <btau20102@yahoo.com>
2016-03-13 14:34:05 -0700

    k

--- Gachatown stuff/Assets/Scripts/GachaToyAI.cs
+++ Gachatown stuff/Assets/Scripts/GachaToyAI.cs
@@ -7,44 +7,71 @@ public class GachaToyAI : MonoBehaviour
     public Transform target;
     public bool isSeeking;
     public bool isWandering;
+    float speed = .02f;
+    Vector3 npcPos;
+    Vector3 targetPos;
+
+    Vector3 kung;
+    Vector3 fu;
+
+    Vector3 wanderDir;
     void Start()
     {
         tr = GetComponent<Transform>();
+        //fu = transform.rotation.eulerAngles;
+        //fu.x = 0;
+        //fu.z = 0;
+        isWandering = false;
+        isSeeking = true;
 
     }
     void Update()
     {
-
+        
+        
+        
     }
     void FixedUpdate()
     {
-        //if (isWandering)
-        //{
-        //    TrueWander();
-        //}     
-        if (isSeeking)
-        {
-            Seek();
-        }
         if (Input.GetKeyDown("space"))
         {
             isWandering = false;
             isSeeking = true;
         }
+        if (Input.GetKeyDown("s"))
+        {
+            isWandering = true;
+            isSeeking = false;
+        }
+        
+        if (isSeeking)
+        {
+            Seek();
+        }
+        if (isWandering)
+        {
+            TrueWander();
+        }
 
     }
     void TrueWander()
     {
-        var foo = transform.rotation.eulerAngles;
-        foo.x = 0;
-        foo.z = 0;
-        foo.y += Random.Range(-3, 5);
-        tr.Translate(transform.forward * .002f);
-        tr.transform.rotation = Quaternion.Euler(foo);
+       wanderDir = transform.rotation.eulerAngles;
+        wanderDir.x = 0;
+        wanderDir.z = 0;
+        wanderDir.y += Random.Range(-5, 7);
+        tr.transform.Translate(transform.forward * speed);
+        tr.transform.rotation = Quaternion.Euler(wanderDir);
     }
     void Seek()
     {
-        tr.Translate(Vector3.Normalize(tr.transform.position -target.transform.position)*.02f);
+        npcPos = new Vector3( tr.transform.position.x, 0, tr.transform.position.z);       
+        targetPos = new Vector3(target.transform.position.x, 0, target.transform.position.z);
+        tr.transform.Translate((Vector3.Normalize(npcPos-targetPos) *speed));
+      //tr.transform.rotation = Quaternion.Euler(fu);
+        
+       //tr.transform.LookAt(target,Vector3.up);
+
     }
 
 
@@ -53,6 +80,14 @@ public class GachaToyAI : MonoBehaviour
         if (myTarget.gameObject.tag == "Target")
         {
             Debug.Log("IHITTHETHING");
+        }
+
+    }
+    void OnCollisionExit(Collision myTarget)
+    {
+        if (myTarget.gameObject.tag == "Target")
+        {
+            Debug.Log("istopped");
         }
 
     }
