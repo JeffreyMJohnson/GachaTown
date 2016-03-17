36b9d7b3c792c25e953a6d058002531aab674d0f
Jeffrey Johnson <jefjohms@gmail.com>
2016-03-17 14:46:48 -0700

    fixed a bug in the buy gacha logic.

--- Assets/_scripts/Player.cs
+++ Assets/_scripts/Player.cs
@@ -7,4 +7,13 @@ public class Player : MonoBehaviour
     public int TotalCoins = 0;
     public List<GameObject> GachaCollection = null;
 
+    public void AddGachaToList(GameObject gacha)
+    {
+        if(GachaCollection == null)
+        {
+            GachaCollection = new List<GameObject>();
+        }
+        GachaCollection.Add(gacha);
+    }
+
 }
--- Assets/brysonscripts/BuyGacha.cs
+++ Assets/brysonscripts/BuyGacha.cs
@@ -2,35 +2,26 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class BuyGacha : MonoBehaviour {
+public class BuyGacha : MonoBehaviour
+{
 
-    [SerializeField]
-    Text money;
-    //GameObject playerObj;
-    //[SerializeField]
-    int moneyInt;
+
+    public Text moneyTextField;
     Player localPlayer;
 
-	void Start () {
+    void Start()
+    {
         GameObject tPlayer = GameObject.FindGameObjectWithTag("Player");
         localPlayer = tPlayer.GetComponent<Player>();
-        moneyInt = localPlayer.TotalCoins;
-        money.text = moneyInt.ToString();
-	}
-	
-	void Update () {
-        
+        moneyTextField.text = localPlayer.TotalCoins.ToString();
     }
-   public void Buy()
+    public void Buy()
     {
-        if (moneyInt >= 5)
+        if (localPlayer.TotalCoins >= 5)
         {
-            GameObject tPlayer = GameObject.FindGameObjectWithTag("Player");
-            moneyInt -= 5;
-            money.text = tPlayer.GetComponent<Player>().TotalCoins.ToString();
-            //localPlayer.TotalCoins = moneyInt;
-            tPlayer.GetComponent<Player>().TotalCoins = moneyInt;
+            localPlayer.TotalCoins -= 5;
+            moneyTextField.text = localPlayer.TotalCoins.ToString();
         }
-        
+
     }
 }
