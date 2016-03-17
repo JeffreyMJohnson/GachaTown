980e9932c6acf11bc186b0395c24a7b04ad4005e
Jeffrey Johnson <jefjohms@gmail.com>
2016-03-16 18:20:03 -0700

    implemented player save/load

---
+++ Assets/_scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Player : MonoBehaviour
+{
+    public int TotalCoins = 0;
+    public List<GameObject> GachaCollection = null;
+}
--- Assets/_scripts/_constants.cs
+++ Assets/_scripts/_constants.cs
@@ -3,5 +3,6 @@
 static class Constants
 {
     public const string LOCAL_DATABASE_PATH = "/Database/TestDB1.db";
+    public const string PLAYER_STATE_PATH = "/Data/Player.state";
 }
 
