f0f7032aec58210dcae2e4871d08700936186dd2
tyler <tyler.miller@students.aie.edu.au>
2016-03-17 10:22:24 -0700

    Merge part uno

---
+++ Assets/_scripts/PlayerLoadSave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+
+
+class PlayerLoadSave : MonoBehaviour
+{
+    Player playerScript;
+
+    void Start()
+    {
+        playerScript = GetComponent<Player>();
+        Debug.Assert(playerScript != null, "Did not find Player script.");
+
+        LoadState();
+    }
+
+    void OnDestroy()
+    {
+        SaveState();
+    }
+
+    void SaveState()
+    {
+        StreamWriter writer = new StreamWriter(Application.dataPath + Constants.PLAYER_STATE_PATH);
+        writer.Write(JsonUtility.ToJson(playerScript));
+        writer.Close();
+    }
+
+    void LoadState()
+    {
+        if (!File.Exists(Application.dataPath + Constants.PLAYER_STATE_PATH))
+        {
+            return;
+        }
+        StreamReader reader = new StreamReader(Application.dataPath + Constants.PLAYER_STATE_PATH);
+
+        JsonUtility.FromJsonOverwrite(reader.ReadToEnd(), playerScript);
+        reader.Close();
+    }
+}
