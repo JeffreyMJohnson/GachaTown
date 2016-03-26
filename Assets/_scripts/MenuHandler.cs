b9d5188bf1159ac4109613b7791114cd18e05a56
Jeffrey Johnson <jefjohms@gmail.com>
2016-03-26 16:43:05 -0700

    reworked UI in 'buy gacha' scene.

--- Assets/_scripts/MenuHandler.cs
+++ Assets/_scripts/MenuHandler.cs
@@ -6,12 +6,7 @@ public class MenuHandler : MonoBehaviour {
     public enum Menus { SPLASH, MAIN, GACHA, TOWN, COLLECTION, SETTING, GACHACHOOSE }
 
     //Menus menuState = 0;
-
-    // Use this for initialization
-    void Start ()
-    {
-
-    }
+    
 
     public void LoadMain()
     {
@@ -48,35 +43,5 @@ public class MenuHandler : MonoBehaviour {
         UnityEngine.SceneManagement.SceneManager.LoadScene((int)aChangeTo);
         //menuState = aChangeTo;
     }
-	// Update is called once per frame
-	void Update ()
-    {
-
-        
-        //Menus ToChange = menuState;
-        //switch (menuState)
-        //{
-        //    case Menus.SPLASH:
-        //        if (Input.anyKey)
-        //        {
-        //            ToChange = Menus.MAIN;
-        //        }
-        //        break;
-        //    case Menus.MAIN:
-        //        break;
-        //    case Menus.GACHA:
-        //        break;
-        //    case Menus.TOWN:
-        //        break;
-        //    case Menus.COLLECTION:
-        //        break;
-        //    case Menus.SETTING:
-        //        break;
-        //    default:
-        //        Debug.Log("invalid menuState");
-        //        break;
-        //}
-        //Change(ToChange);
-
-    }
+	
 }
