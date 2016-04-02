25f4a17e7b9c226043b337634a5b3e84210ec747
Jeffrey Johnson <jefjohms@gmail.com>
2016-04-02 12:57:09 -0700

    implemented screen orientation change for main menu scene.

--- Assets/_scripts/MainMenu.cs
+++ Assets/_scripts/MainMenu.cs
@@ -1,11 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class MainMenu : MonoBehaviour
 {
+    public UnityEvent orientationChangeEvent;
+
+    private ScreenOrientation currentOrientation;
+    Canvas portrait;
+    Canvas landscape;
+
+
+
     void Start()
     {
         InitButtonHandlers();
+
+        currentOrientation = Screen.orientation;
+
+        if(orientationChangeEvent == null)
+        {
+            orientationChangeEvent = new UnityEvent();
+
+        }
+
+        orientationChangeEvent.AddListener(HandleScreenOrientationChange);
+
+        InitCanvas();
+       
+
     }
 
     public void HandleClick(GameManager.Menus scene)
@@ -19,11 +42,19 @@ public class MainMenu : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if(Screen.orientation != currentOrientation)
+        {
+            currentOrientation = Screen.orientation;
+            orientationChangeEvent.Invoke();
+        }
     }
 
     void InitButtonHandlers()
     {
-        Button[] buttons = GetComponentsInChildren<Button>();
+
+
+        Button[] buttons = FindObjectsOfType<Button>();
 
         foreach (Button button in buttons)
         {
@@ -48,4 +79,45 @@ public class MainMenu : MonoBehaviour
         }
     }
 
+    #region Handle Screen Orientation Change
+    void InitCanvas()
+    {
+        Canvas[] canvasSiblings = FindObjectsOfType<Canvas>();
+        foreach (Canvas canvas in canvasSiblings)
+        {
+            if (canvas.name == "Canvas Portrait")
+            {
+                portrait = canvas;
+            }
+            else
+            {
+                landscape = canvas;
+            }
+        }
+        Debug.Assert(landscape != null && portrait != null, "Landscape and Portrait canvas' did not initialize.");
+        SetOrientationCanvas();
+    }
+
+    void SetOrientationCanvas()
+    {
+        if(currentOrientation == ScreenOrientation.Portrait)
+        {
+            portrait.gameObject.SetActive(true);
+            landscape.gameObject.SetActive(false);
+        }
+        else
+        {
+            portrait.gameObject.SetActive(false);
+            landscape.gameObject.SetActive(true);
+        }
+    }
+
+    
+
+    void HandleScreenOrientationChange()
+    {
+        Debug.Log(string.Format("Orientation changed. Now - {0}", Screen.orientation));
+        SetOrientationCanvas();
+    }
+    #endregion
 }
