3303673c1d6a3c5b333ffb964b79c1335ad32642
Jeffrey Johnson <jefjohms@gmail.com>
2016-04-06 19:12:04 -0700

    added pause before switching scenes to allow for sound effects to work.

--- Assets/_scripts/_constants.cs
+++ Assets/_scripts/_constants.cs
@@ -5,5 +5,6 @@ static class Constants
     public const string LOCAL_DATABASE_PATH = "/Database/TestDB1.db";
     public const string PLAYER_STATE_PATH = "/Player.state";
     public const string GACHA_PATH = "Gachas/";
+    public const float SCENE_CHANGE_WAIT_TIME = .05f;
 }
 
