3a16b8a1eb8231770ae8af7d28febf882d1fdcaa
Jeffrey Johnson <jefjohms@gmail.com>
2016-05-05 15:21:51 -0700

    changed town scene gacha placement UI and code.

---
+++ Assets/_scripts/GachaUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class GachaUI : MonoBehaviour,
+    IBeginDragHandler,
+    IDragHandler,
+    IEndDragHandler
+{
+    public GachaID ID { get; set; }
+    public class OnGachaDragEvent : UnityEvent<GachaID> { }
+    public class OnGachaDropEvent : UnityEvent<PointerEventData> { }
+    public OnGachaDragEvent onGachaDrag = new OnGachaDragEvent();
+    public OnGachaDropEvent onGachaDrop = new OnGachaDropEvent();
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GameObject clickedObject = eventData.pointerDrag;
+        GachaUI script = clickedObject.GetComponent<GachaUI>();
+        onGachaDrag.Invoke(script.ID);
+    }
+
+    public void OnDrag(PointerEventData eventData) { }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        onGachaDrop.Invoke(eventData);
+    }
+}
